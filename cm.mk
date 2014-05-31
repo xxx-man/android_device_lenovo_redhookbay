80e6ae0d0994cdb444cf9c12247cf933e8728006
xxx-man <xxx-man@open.by>
2014-05-31 19:37:14 +0300

    mkvendor.sh: Initial commit of k900

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := k900
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/Lenovo/k900/device_k900.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := k900
+PRODUCT_NAME := cm_k900
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := k900
+PRODUCT_MANUFACTURER := Lenovo
